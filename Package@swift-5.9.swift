6d6ce0e8a1ab0f0ebd51e7c9e90a4175de4f336f
Minsung Kim <mskim4048@naver.com>
2025-08-09 18:54:49 +0900

    feat: Add Package@swift-5.9.swift file

---
+++ Package@swift-5.9.swift
@@ -0,0 +1,25 @@
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "Wisp",
+    platforms: [.iOS(.v15)],
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "Wisp",
+            targets: ["Wisp"]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(name: "Wisp", path: "Sources"),
+        .testTarget(
+            name: "WispTests",
+            dependencies: ["Wisp"]
+        ),
+    ],
+    swiftLanguageVersions: [.v5]
+)
