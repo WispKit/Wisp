ce00f4d50c2fd04b1db0a715f43484037d8b37b6
Minsung Kim <mskim4048@naver.com>
2025-09-15 20:22:06 +0900

    feat: add public API entry point via UICollectionViewCompositionalLayout extension and separate WispLayoutMaker implementation

---
+++ Sources/Collection View/Layout/WispLayoutMaker.swift
@@ -0,0 +1,66 @@
+//
+//  File.swift
+//  Wisp
+//
+//  Created by 김민성 on 9/15/25.
+//
+
+import UIKit
+
+public protocol WispLayoutMakerType {
+    
+    @MainActor
+    func make(section: NSCollectionLayoutSection) -> WispCompositionalLayout
+    
+    @MainActor
+    func make(
+        section: NSCollectionLayoutSection,
+        configuration: UICollectionViewCompositionalLayoutConfiguration
+    ) -> WispCompositionalLayout
+    
+    @MainActor
+    func make(
+        sectionProvider: @escaping UICollectionViewCompositionalLayoutSectionProvider
+    ) -> WispCompositionalLayout
+    
+    @MainActor
+    func make(
+        sectionProvider: @escaping UICollectionViewCompositionalLayoutSectionProvider,
+        configuration: UICollectionViewCompositionalLayoutConfiguration
+    ) -> WispCompositionalLayout
+    
+}
+
+
+/// Concrete type of `WispLayoutMakerType`
+internal struct WispLayoutMaker: WispLayoutMakerType {
+    
+    @MainActor
+    public func make(section: NSCollectionLayoutSection) -> WispCompositionalLayout {
+        return WispCompositionalLayout(section: section)
+    }
+    
+    @MainActor
+    public func make(
+        section: NSCollectionLayoutSection,
+        configuration: UICollectionViewCompositionalLayoutConfiguration
+    ) -> WispCompositionalLayout {
+        return WispCompositionalLayout(section: section, configuration: configuration)
+    }
+    
+    @MainActor
+    public func make(
+        sectionProvider: @escaping UICollectionViewCompositionalLayoutSectionProvider
+    ) -> WispCompositionalLayout {
+        return WispCompositionalLayout(sectionProvider: sectionProvider)
+    }
+    
+    @MainActor
+    public func make(
+        sectionProvider: @escaping UICollectionViewCompositionalLayoutSectionProvider,
+        configuration: UICollectionViewCompositionalLayoutConfiguration
+    ) -> WispCompositionalLayout {
+        return WispCompositionalLayout(sectionProvider: sectionProvider, configuration: configuration)
+    }
+    
+}
