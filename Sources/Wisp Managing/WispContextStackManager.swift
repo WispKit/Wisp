421f4d5fd183be31cc7e9e09c25ebe131dd6a51d
Minsung Kim <mskim4048@naver.com>
2025-10-03 16:55:00 +0900

    feat: Add validation when retrieving current context from stack

--- Sources/Wisp Managing/WispContextStackManager.swift
+++ Sources/Wisp Managing/WispContextStackManager.swift
@@ -13,7 +13,7 @@ import Foundation
 
     var currentContext: WispContext? {
         get {
-            return stack.last
+            return getValidCurrentContext()
         }
         set {
             if let newValue {
@@ -29,7 +29,26 @@ import Foundation
         stack.append(context)
     }
     
+    @discardableResult
     func pop() -> WispContext? {
         return stack.popLast()
+    }
+    
+    /// Returns the topmost valid `WispContext` from the stack.
+    ///
+    /// If the most recent context is invalid (its `viewControllerToPresent`is no longer available),
+    /// the method removes it and continues checking down the stack until a valid context is found or the stack is empty.
+    ///
+    /// - Returns: The topmost valid `WispContext`, or `nil` if the stack is empty.
+    private func getValidCurrentContext() -> WispContext? {
+        guard let lastItem = stack.last else {
+            return nil
+        }
+        if lastItem.viewControllerToPresent?.presentingViewController != nil {
+            return lastItem
+        } else {
+            self.pop()
+            return getValidCurrentContext()
+        }
     }
 }
