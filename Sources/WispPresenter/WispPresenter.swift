063ccae7067f2782e182a85597e5463680503d9a
Minsung Kim <mskim4048@naver.com>
2025-10-12 23:10:39 +0900

    fix: throw fatalError when collectionView is not a subview of presenting view controller

--- Sources/WispPresenter/WispPresenter.swift
+++ Sources/WispPresenter/WispPresenter.swift
@@ -46,19 +46,10 @@ public extension WispPresenter {
     ) {
         guard let hostViewController else { return }
         guard hostViewController.view.containsAsSubview(collectionView) else {
-            print("Collection view is not subview of presenting view controller's view.")
-            print("Custom transition will be cancelled and run modal presentation.")
-            viewControllerToPresent.modalPresentationStyle = .formSheet
-            hostViewController.present(viewControllerToPresent, animated: true)
-            return
+            fatalError("""
+            WispPresenter Error: The collection view must be a subview of the presenting view controller's view.
+            """)
         }
-        
-        /// 바로 직전의 예외처리로 인한 modal presentation을 `dismiss`하고 난 직후 바로 다른 VC를 `present`하려고 할 때,
-        /// `dismiss`가 완전히 끝나기 전인 경우,
-        /// (`present`되는 시점에 `presentingViewController`의 `presentedViewController` 가 존재하면)
-        /// 의도한 custom transition 대신 `.fullScreen` 방식으로 transition되는 문제 발생.
-        /// 시스템이 자동으로 fullscreen transition을 채택하는 것 같다.
-//        guard sourceViewController.presentedViewController == nil else { return }
         
         let selectedCell = collectionView.cellForItem(at: indexPath)
         let cellSnapshot = selectedCell?.snapshotView(afterScreenUpdates: false)
